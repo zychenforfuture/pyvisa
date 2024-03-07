67e04be98bb4ee160b8a5136bd2f1f0ede75d6a3
zychenforfuture <czyforfuture@163.com>
2024-03-07 17:35:16 +0800

    Sweep

--- WK6500/def_CpG_Sweep_meas.py
+++ WK6500/def_CpG_Sweep_meas.py
@@ -188,4 +188,13 @@ def WK6500B_CpG_Sweep_meas(Level, Speed, V_list, Freq_list, DevName, SweepLoop, 
         plt.savefig(Fig_res, format='jpeg')
 
     return res
->>>>>>> before_del_data
+if __name__ == '__main__':
+    Level = 1
+    Speed = 1
+    V_list = np.linspace(-1, 1, 11)
+    Freq_list = np.logspace(1, 6, 6)
+    DevName = 'WK6500B'
+    SweepLoop = 'v'
+    Fig = 1
+    res = WK6500B_CpG_Sweep_meas(Level, Speed, V_list, Freq_list, DevName, SweepLoop, Fig)
+    print(res)
