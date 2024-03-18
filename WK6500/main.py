4089d7c9ec4ce37891a2189f5b0bc725857ac908
zychenforfuture <czyforfuture@163.com>
2024-03-18 15:18:20 +0800

    添加Fig注释

--- WK6500/main.py
+++ WK6500/main.py
@@ -10,7 +10,7 @@ import pyvisa
 DevName = '3D_CV_3V 100 kHz'
 # AC setting
 Level = 30e-3  # AC level
-Speed = 'SLOW'  # measurement speed: MAXimum; FAST; MEDium; SLOW;
+Speed = 'FAST'  # measurement speed: MAX; FAST; MED; SLOW;
 
 # Sweep loop (inner loop) and Step Loop (outer loop) in a nested for loop
 SweepLoop = 'v'  # SweepLoop = 'V' or 'F'
@@ -18,7 +18,7 @@ SweepLoop = 'v'  # SweepLoop = 'V' or 'F'
 # Frequency step settings
 # Freq_list = [1e3, 1e4, 1e5]  # Frequency list
 Fstart = 1e5
-Fstop = 1e5 #Maximum frequency = 1e6
+Fstop = 1e6  # Maximum frequency = 1e6
 PointsPerDecade = 1  # number of points per decade
 # frequency list generation with uniform distribution in log scale
 Freq_list = F_list_gen(Fstart, Fstop, PointsPerDecade)
@@ -27,11 +27,13 @@ print(Freq_list)
 # Voltage sweep settings
 Vstart = -3
 Vstop = 3
-Vstep = 0.05 # Must be divisible by Vstop - Vstart, preferably divisible by Vstop
+Vstep = 0.05  # Must be divisible by Vstop - Vstart, preferably divisible by Vstop
 isHys = 1  # 1, measure hysteresis; 0, no hysteresis
 # voltage list generation
-V_list = V_list_gen(Vstart, Vstop, Vstep, isHys)  
-print(V_list)  
+V_list = V_list_gen(Vstart, Vstop, Vstep, isHys)
+print(V_list)
+
+Fig = 1  # 1 to plot 0 none
 
 # Impedance sweep measurements
-WK6500B_CpG_Sweep_meas(Level, Speed, V_list, Freq_list, DevName, SweepLoop, Fig=1)
+WK6500B_CpG_Sweep_meas(Level, Speed, V_list, Freq_list, DevName, SweepLoop, Fig)
