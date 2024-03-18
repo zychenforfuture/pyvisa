a6d98aa30fb2fd6072a9061db32ae0c77fca8fb2
zychenforfuture <czyforfuture@163.com>
2024-03-18 19:08:32 +0800

    HZO-4V-20040318

--- WK6500/main.py
+++ WK6500/main.py
@@ -7,17 +7,17 @@ from def_CpG_Sweep_meas import WK6500B_CpG_Sweep_meas
 import pyvisa
 
 # Name setting
-DevName = '3D_CV_3V 100 kHz'
+DevName = 'HZO_CV_4V'
 # AC setting
 Level = 30e-3  # AC level
-Speed = 'FAST'  # measurement speed: MAX; FAST; MED; SLOW;
+Speed = 'MED'  # measurement speed: MAX; FAST; MED; SLOW;
 
 # Sweep loop (inner loop) and Step Loop (outer loop) in a nested for loop
 SweepLoop = 'v'  # SweepLoop = 'V' or 'F'
 
 # Frequency step settings
 # Freq_list = [1e3, 1e4, 1e5]  # Frequency list
-Fstart = 1e5
+Fstart = 1e3
 Fstop = 1e6  # Maximum frequency = 1e6
 PointsPerDecade = 1  # number of points per decade
 # frequency list generation with uniform distribution in log scale
@@ -25,8 +25,8 @@ Freq_list = F_list_gen(Fstart, Fstop, PointsPerDecade)
 print(Freq_list)
 
 # Voltage sweep settings
-Vstart = -3
-Vstop = 3
+Vstart = -4
+Vstop = 4
 Vstep = 0.05  # Must be divisible by Vstop - Vstart, preferably divisible by Vstop
 isHys = 1  # 1, measure hysteresis; 0, no hysteresis
 # voltage list generation
